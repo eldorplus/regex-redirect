1d29842eb5cd26d149eab8403d7e403996013033
Patrick LUZOLO <eldorplus@gmail.com>
2024-02-13 18:02:24 +0100

    Update project

--- public/regex-redirect.js
+++ public/regex-redirect.js
@@ -1,96 +1,144 @@
-let prefix = "[regex-redirect]";
-let log    = (...args) => console.log(  prefix, ...args);
-let err    = (...args) => console.error(prefix, ...args);
+const prefix = "[regex-redirect]";
+const log = (...args) => console.log(prefix, ...args);
+const err = (...args) => console.error(prefix, ...args);
 
-let whitelistHosts = [];
-let table   = document.querySelector("#redirect_table");
-let new_btn = document.querySelector("#new_entry_btn");
-
-// Load entries into table
-browser.storage.local.get({ entries: [] })
-    .then(storage => {
-	populateTable(storage.entries);
-    }, err);
-
-// Save entries once pop-up is unloaded
-window.addEventListener("unload", () => {
-    let entries = [];
-    
-    for(let row of table.querySelectorAll("tr:not(#header)")) {
-	let src = row.querySelector(".source").value;
-	let tgt = row.querySelector(".target").value;
-
-	if(src && tgt) {
-	    entries.push({
-		source: src,
-		target: tgt
-	    });
-	}
-    }
-
-    browser.storage.local.set({ entries: entries })
-	.then(() => log("Saved entries"), err);
-});
-
-// Add a new row to the table
-new_btn.onclick = () => addTableEntry();
-
-function populateTable(entries) {
-    if(!table) return err("Invalid redirect table");
-    
-    for(let entry of entries)
-	addTableEntry(entry.source, entry.target);
-
-    addTableEntry();
-}
-
-function addTableEntry(source="", target="") {
-    if(!table) return err("Invalid redirect table");
-
-    let row  = table.insertRow(-1);
-    let col1 = row.insertCell(-1);
-    let col2 = row.insertCell(-1);
-    let col3 = row.insertCell(-1);
-    let inp1 = document.createElement("input");
-    let inp2 = document.createElement("input");
-    let inp3 = document.createElement("button");
-    
-    inp1.value = source;
-    inp2.value = target;
-
-    inp1.classList.add("source");
-    inp2.classList.add("target");
-    inp3.classList.add("delete");
-    
-    inp3.onclick = () => row.remove();
-    inp3.innerText = "X";
-    
-    col1.append(inp1);
-    col2.append(inp2);
-    col3.append(inp3);
-}
+const whitelistHosts = {
+    'stiboapp.dev.pointp.saint-gobain.net': 'sgdbf-dev.mdm.stibosystems.com',
+    'stiboapp.int.pointp.saint-gobain.net': 'sgdbf-int.mdm.stibosystems.com',
+    'stiboapp.recette.pointp.saint-gobain.net': 'sgdbf-qa.mdm.stibosystems.com',
+    'stiboapp.preprod.pointp.saint-gobain.net': 'sgdbf-ppd.mdm.stibosystems.com',
+    'stiboapp.prod.pointp.saint-gobain.net': 'sgdbf-prod.mdm.stibosystems.com'
+};
 
 /*
-
-function convertTargetPattern(source, target) {
-    let url = new URL(source);
-
-    url.tld       = url.hostname.match("\.[^\.]+$")[0];
-    url.domain    = url.hostname.replace(url.tld, "").match("[^\.]+$")[0];
-    url.subdomain = url.hostname.replace(url.tld, "").replace(url.domain, "");
-
-    let result = "";
-
-    for(let x of target.split("]")) {
-	if(x.includes("[")) {
-	    let index = x.indexOf("[");
-
-	    result += x.substr(0, index) + url[x.substr(index + 1)];
-	} else {
-	    result += x;
-	}
-    }
-
-    return result;
+{
+    "ancestorOrigins": {
+        "0": "https://codepen.io"
+    },
+    "href": "https://cdpn.io/cpe/boomboom/index.html?editors=0012&key=index.html-f1981af8-7dc2-f8b6-669a-8980d4a8d02a",
+    "origin": "https://cdpn.io",
+    "protocol": "https:",
+    "host": "cdpn.io",
+    "hostname": "cdpn.io",
+    "port": "",
+    "pathname": "/cpe/boomboom/index.html",
+    "search": "?editors=0012&key=index.html-f1981af8-7dc2-f8b6-669a-8980d4a8d02a",
+    "hash": ""
 }
 */
+
+// Obtain the root
+const rootNode = document.getElementById("redirect-entry-point");
+const isLocalHost = hostname => !!(
+    hostname === 'localhost' ||
+    hostname === '[::1]' ||
+    hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
+);
+
+const useOutsideClick = (ref, callback) => {
+    React.useEffect(() => {
+        const listener = (event) => {
+            const isClickedOutside =
+                !ref.current.contains(event.target) &&
+                !document.getElementById('gtx-trans').contains(event.target);
+            if (isClickedOutside) {
+                callback();
+            }
+        };
+
+        document.addEventListener('mousedown', listener);
+        document.addEventListener('touchstart', listener);
+
+        return () => {
+            document.removeEventListener('mousedown', listener);
+            document.removeEventListener('touchstart', listener);
+        };
+    }, [ref]);
+};
+
+const HttpsRedirect = ({disabled, children}) => {
+    if (
+        !disabled &&
+        typeof window !== 'undefined' &&
+        window.location &&
+        window.location.protocol === 'http:' &&
+        !isLocalHost(window.location.hostname)
+    ) {
+        window.location.href = window.location.href.replace(
+            /^http(?!s)/,
+            'https'
+        );
+        return null;
+    }
+
+    return children;
+};
+
+const Redirect = ({domain, protocol}) => {
+
+    const containerRef = React.useRef(null);
+    const [isClicked, setIsClicked] = React.useState(false);
+
+    useOutsideClick(containerRef, () => {
+        setIsClicked(false);
+    });
+
+    const handerRedirect = React.useCallback(async () => {
+        let redirectUrl = protocol.replace(
+            /^http(?!s)/,
+            'https'
+        ).concat('//').concat(domain);
+        log("shout be redirect ==>", redirectUrl)
+        setIsClicked(!isClicked);
+        const timeout = setTimeout(() => {
+            // 👇️ redirects to an external URL
+            window.location.replace(redirectUrl);
+        }, 3000);
+
+        return () => clearTimeout(timeout);
+    }, [isClicked])
+
+    return (
+        <div className="relative" ref={containerRef}>
+            {!isClicked && (<button onClick={handerRedirect}>Redirect to {domain}</button>)}
+            {isClicked && (<div>Will redirect in 3 seconds...</div>)}
+        </div>
+    );
+};
+
+const Welcome = ({name}) => {
+    // Use the render function to return JSX component
+    return (
+        <div className="welcome">
+            <h1>This {name} will be deactivated in 6 months.</h1>
+            <p>Please click here</p>
+        </div>
+    );
+}
+
+
+// https://github.com/sospedra/next.js/blob/canary/packages/next/client/link.tsx
+let {pathname, hostname, origin, search, protocol} = window.location
+let domain = hostname;
+
+if (Object.hasOwn(whitelistHosts, hostname)) {
+    log(pathname, hostname)
+    domain = whitelistHosts[hostname]
+    log(domain);
+}
+
+// Create a function to wrap up your component
+const App = (props) => {
+
+    return (
+        <HttpsRedirect>
+            <div>
+                <Welcome name={hostname}/>
+                <Redirect domain={domain} protocol={protocol}/>
+            </div>
+        </HttpsRedirect>
+    );
+}
+
+// Use the ReactDOM.render to show your component on the browser
+ReactDOM.render(<App/>, rootNode)
